f0f81fa79fb918e5c326e874540c85753130511d
Gregory Fischer <gwhoisj@gmail.com>
2021-12-09 14:59:09 -0500

    Feat: search works, but doesn't match postman requirements

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
+      get 'items/find', to: 'items#find'
+      get 'items/find_all', to: 'items#find_all'
+
+      get 'merchants/find', to: 'merchants#find'
+      get 'merchants/find_all', to: 'merchants#find_all'
+
       resources :merchants, only: [:index, :show]
 
       resources :items
