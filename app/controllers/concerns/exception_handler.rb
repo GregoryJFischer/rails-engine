69fc2fbc21c9c462d20923dd6ef7891609859731
Gregory Fischer <gwhoisj@gmail.com>
2021-12-09 17:51:14 -0500

    Feat: change error message to match postman suite

--- app/controllers/concerns/exception_handler.rb
+++ app/controllers/concerns/exception_handler.rb
@@ -3,7 +3,7 @@ module ExceptionHandler
 
   included do
     rescue_from ActiveRecord::RecordNotFound do |e|
-      render json: {message: e.message}, status: 404
+      render json: {error: e.message}, status: 404
     end
   end
 end
